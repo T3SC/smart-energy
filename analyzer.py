29f083de1d5bde9c593687cb2a663433f4618e76
Umair Akhtar <omayr.akhtar@gmail.com>
2017-07-25 12:37:55 +0300

    2-3 lstm experimentation

--- analyzer.py
+++ analyzer.py
@@ -11,6 +11,18 @@ import numpy as np
 import time, datetime
 from os import listdir
 from os import path
+import matplotlib.pyplot as plt
+import math
+from keras.models import Sequential
+from keras.layers.wrappers import TimeDistributed
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.layers.convolutional import Conv1D, MaxPooling1D
+from keras.layers.core import Dense, Activation, Dropout, RepeatVector
+from keras.layers.advanced_activations import PReLU
+from keras.layers.normalization import BatchNormalization
+from sklearn.externals import joblib
 
 def read_file():
 
@@ -109,19 +121,119 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     # drop rows with NaN values
     if dropnan:
         agg.dropna(inplace=True)
+
     return agg
+
+def split_test_train(ts_univariate, split, look_back):
+
+    # split into train and test sets
+    train_size = int(look_back * (split*100))
+
+    train = ts_univariate[0:train_size, :]
+    test = ts_univariate[train_size:len(ts_univariate)-1, :]
+    train_X, train_y = train[:, 0:look_back], train[:, look_back:]
+    test_X, test_y = test[:,0:look_back], test[:,look_back:]
+
+    return train_X, train_y, test_X, test_y
+
 
 def main():
 
-    read_file()
-    # data = pd.read_csv("data/sample/sample.csv")
-    #
-    #
-    # univariate = data['RT']
-    # print len(univariate)
-    #
-    # ts_univariate = series_to_supervised(univariate,288,288)
-    # print len(list(ts_univariate))
+    np.random.seed(7)
+    split = 0.8
+    look_back = 288
+    data = pd.read_csv("data/sample/sample.csv")
+
+
+    # loading and normalizing univariate data
+    univariate = data['RT'].values
+
+
+    # Filling missing values.
+    avg = data['RT'].mean()
+    univariate[np.isnan(univariate)] = avg
+    #univariate[np.isnan(univariate)] = 0
+
+    #univariate = univariate.astype('float32')
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    univariate = scaler.fit_transform(univariate)
+    univariate = univariate.tolist()
+    ts_univariate = series_to_supervised(univariate,look_back,look_back)
+    ts_univariate = np.array(ts_univariate)
+
+    train_X, train_y, test_X, test_y = split_test_train(ts_univariate, split, look_back)
+
+    # reshape input to be [samples, time steps, features]
+    train_X = np.reshape(train_X, (train_X.shape[0],1, train_X.shape[1]))
+    test_X = np.reshape(test_X, (test_X.shape[0],1, test_X.shape[1]))
+
+    # Model
+    model = Sequential()
+
+    # LSTM
+    model.add(LSTM(look_back+1, batch_input_shape=(look_back, train_X.shape[1], train_X.shape[2]), stateful=True, return_sequences=True,))
+    model.add(LSTM(look_back, return_sequences=True, stateful=True))
+    model.add(LSTM(look_back, stateful=True))
+
+    #model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Dense(look_back)) #train_y.shape[1]
+    model.add(Activation('relu'))
+
+    #model.add(RepeatVector(look_back))
+    #model.add(LSTM(look_back, stateful=True))
+    #model.add(Dense(look_back))
+
+    model.compile(loss='mean_squared_error', optimizer='rmsprop')
+
+    # fit network
+    for i in range(100):
+        print "Epoch:",i+1
+        model.fit(np.concatenate((train_X,test_X),axis=0), np.concatenate((train_y,test_y),axis=0), epochs=1, batch_size=look_back, verbose=1, shuffle=False)
+        model.reset_states()
+
+    model_json = model.to_json()
+    with open("data/model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("data/model.h5")
+    print("Saved model to disk")
+
+    # make predictions
+    trainPredict = model.predict(train_X,batch_size=look_back)
+    testPredict = model.predict(test_X,batch_size=look_back)
+
+    # invert predictions
+    trainPredict = scaler.inverse_transform(trainPredict)
+    train_y = scaler.inverse_transform(train_y)
+    testPredict = scaler.inverse_transform(testPredict)
+    test_y = scaler.inverse_transform(test_y)
+
+    for i in range(0,test_y.shape[1]):
+        print test_y[len(test_y)-1,i], testPredict[len(testPredict)-1,i]
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+    ax.plot(test_y[len(test_y)-1,:], 'g', label='Actual Temperature')
+    ax.plot(testPredict[len(testPredict)-1,:], 'r', label='Predicted Temperature')
+    plt.title('Actual Room Temperature vs. Predicted Room Temperature')
+    plt.axis([0, 300, 20, 25])
+    ax.legend(loc='best')
+
+
+    # calculate root mean squared error
+    train_score, test_score = [], []
+
+    for i in range(0,len(train_y)):
+        train_score.append(math.sqrt(mean_squared_error(train_y[i,:], trainPredict[i,:])))
+    for i in range(0, len(test_y)):
+        test_score.append(math.sqrt(mean_squared_error(test_y[i,:], testPredict[i,:])))
+
+    print('Train Score: %.4f RMSE' % (np.mean(train_score)))
+    print('Test Score: %.4f RMSE' % (np.mean(test_score)))
+    plt.show()
+
 
 if __name__ == "__main__": main()
 
